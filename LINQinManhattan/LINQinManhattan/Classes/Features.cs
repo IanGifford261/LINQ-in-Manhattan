20245225f61f07bdea0adf9f93310dc18759fb31
iangifford <lightyagamie10@gmail.com>
2019-03-31 14:49:17 -0700

    features

--- LINQinManhattan/LINQinManhattan/Classes/Features.cs
+++ LINQinManhattan/LINQinManhattan/Classes/Features.cs
@@ -6,6 +6,8 @@ namespace LINQinManhattan.Classes
 {
     public class Features
     {
-
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; } 
+        public Properties Properties { get; set; }
     }
 }
